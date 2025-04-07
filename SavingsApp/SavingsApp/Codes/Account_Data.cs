671df32f451b077a005b5a06577c91bce9e35c46
Ume <79046153+EarthGMS@users.noreply.github.com>
2025-04-07 12:19:56 +0700

    update 4

---
+++ SavingsApp/SavingsApp/Codes/Account_Data.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SavingsApp.Codes
+{
+    class Account_Data
+    {
+        public long MoneyVolume = 3000;
+
+        public void CalculateTransaction(int moneyTransaction)
+        {
+            MoneyVolume += moneyTransaction;
+        }
+    }
+}
--- SavingsApp/SavingsApp/Forms/TransactionForm.cs
+++ SavingsApp/SavingsApp/Forms/TransactionForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SavingsApp.Codes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,14 +13,17 @@ namespace SavingsApp
 {
     public partial class TransactionForm: Form
     {
+        Account_Data account;
         public TransactionForm()
         {
             InitializeComponent();
+            account = new Account_Data();
         }
 
         private void AcceptForm_Click(object sender, EventArgs e)
         {
             Hide();
+            account.CalculateTransaction(int.Parse(TransactionValue.Text));
         }
     }
 }
